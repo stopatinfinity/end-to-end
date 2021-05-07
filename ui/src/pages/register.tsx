7ea67098419f1dd433c6be22e889a75e721789d5
stopatinfinity <nevinchoi@gmail.com>
2021-05-07 18:29:30 -0400

    add submit button

--- ui/src/pages/register.tsx
+++ ui/src/pages/register.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Formik, Form } from 'formik'
-import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 
@@ -8,33 +8,43 @@ interface registerProps {
 
 };
 
-const Register: React.FC<registerProps> = ({}) => {
+const Register: React.FC<registerProps> = ({ }) => {
     return (
         <Wrapper variant='small'>
-            <Formik initialValues={{username: '', password: ""}} 
-            onSubmit={(values)=>{
-                console.log(values);
-            }}>
+            <Formik initialValues={{ username: '', password: "" }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}>
                 {
-                    ({values, handleChange}) => (
+                    ({ isSubmitting }) => (
                         <Form>
-                            <InputField 
+                            <InputField
                                 name='username'
-                                placeholder="username" 
+                                placeholder="username"
                                 label="Username"
                             />
-                            <InputField 
-                                name='password' 
-                                placeholder="password" 
-                                label="Password" 
-                                type="password"
-                            />
+                            <Box mt={4}>
+                                <InputField
+                                    name='password'
+                                    placeholder="password"
+                                    label="Password"
+                                    type="password"
+                                />
+                            </Box>
+                            <Button 
+                                mt={4} 
+                                type="submit" 
+                                isLoading={isSubmitting} 
+                                colorScheme="teal"
+                            >
+                                register
+                            </Button>
                         </Form>
                     )
                 }
             </Formik>
         </Wrapper>
     );
-} 
+}
 
 export default Register;
