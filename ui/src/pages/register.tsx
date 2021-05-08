57b15c75fcc910665114b90a467b54cac5f17eeb
stopatinfinity <nevinchoi@gmail.com>
2021-05-07 22:12:25 -0400

    add graphql codegen and register error handling

--- ui/src/pages/register.tsx
+++ ui/src/pages/register.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Formik, Form } from 'formik'
-import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from '@chakra-ui/react';
+import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button, useMenuState } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useMutation } from "urql";
+import { useRegisterMutation } from '../generated/graphql';
+import { response } from 'express';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router'
 
-interface registerProps {
-
-};
+interface registerProps {};
 
 const Register: React.FC<registerProps> = ({ }) => {
+    const router = useRouter();
+    const [, register] = useRegisterMutation();
     return (
         <Wrapper variant='small'>
-            <Formik initialValues={{ username: '', password: "" }}
-                onSubmit={(values) => {
-                    console.log(values);
+            <Formik initialValues={{ username: "", password: "" }}
+                onSubmit={async (values, { setErrors }) => {
+                    const response = await register(values);
+                    if(response.data?.register.errors) {
+                        setErrors(toErrorMap(response.data.register.errors));
+                    } else if (response.data?.register.user) {
+                        // worked
+                        router.push('/');
+                    }
                 }}>
                 {
                     ({ isSubmitting }) => (
