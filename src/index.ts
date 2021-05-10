fb5aae10ce459bd96ea0b0a1c3a816103e8d8cae
stopatinfinity <nevinchoi@gmail.com>
2021-05-09 21:04:50 -0400

    add server-side rendering

--- src/index.ts
+++ src/index.ts
@@ -2,67 +2,68 @@ import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { COOKIE_NAME, __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
 
-import redis from 'redis';
-import session from 'express-session';
-import connectRedis from 'connect-redis'
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 import { MyContext } from "./types";
 import cors from "cors";
 
-
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-    const app = express();
+  const app = express();
 
-    const RedisStore = connectRedis(session);
-    const redisClient = redis.createClient();
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
 
-    app.use(cors({
-        origin: "http://localhost:3000",
-        credentials:true,
-    }));
-    app.use(
-        session({
-            name: COOKIE_NAME,
-            store: new RedisStore({ 
-                client: redisClient,
-                disableTouch: true
-            }),
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
-                httpOnly: true,
-                sameSite: 'lax',
-                secure: __prod__ // only in https
-            },
-            saveUninitialized: false,
-            secret: 'keyboard cat',
-            resave: false,
-        })
-    );
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        sameSite: "lax",
+        secure: __prod__, // only in https
+      },
+      saveUninitialized: false,
+      secret: "keyboard cat",
+      resave: false,
+    })
+  );
 
-    const apolloServer = new ApolloServer({
-        schema: await buildSchema({
-            resolvers: [HelloResolver, PostResolver, UserResolver],
-            validate: false
-        }),
-        context: ({req, res}): MyContext => ({ em:  orm.em, req, res }),
-    });
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  });
 
-    apolloServer.applyMiddleware({ app, cors: false });
+  apolloServer.applyMiddleware({ app, cors: false });
 
-    app.listen(4000, () => {
-        console.log('server started on localhost:4000');
-    });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
-    console.error(err);
+  console.error(err);
 });
--- ui/src/components/NavBar.tsx
+++ ui/src/components/NavBar.tsx
@@ -1,18 +1,21 @@
 import { Box, Button, Flex, Link } from '@chakra-ui/react'
 import NextLink from 'next/link'                    // so we can do client-side routing
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
+import { isServer } from '../utils/isServer';
 
-interface NavBarProps {}
+interface NavBarProps { }
 
-export const NavBar: React.FC<NavBarProps> = ({}) => {
-    const [{fetching: logoutFetching}, Logout] = useLogoutMutation();
-    const [{data, fetching}] = useMeQuery();
+export const NavBar: React.FC<NavBarProps> = ({ }) => {
+    const [{ fetching: logoutFetching }, Logout] = useLogoutMutation();
+    const [{ data, fetching }] = useMeQuery({
+        pause: isServer()
+    });
     let body = null;
 
     // data loading 
-    if(fetching) {
+    if (fetching) {
         body = null;
-    } else if(!data?.me) { // user not logged in
+    } else if (!data?.me) { // user not logged in
         body = (
             <>
                 <NextLink href="/login">
@@ -27,13 +30,13 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
         body = (
             <Flex>
                 <Box mr={2}>{data.me.username}</Box>
-                <Button 
+                <Button
                     onClick={() => {
                         Logout();
-                    }} 
+                    }}
                     isLoading={logoutFetching}
                     variant="link">
-                        logout
+                    logout
                 </Button>
             </Flex>
         )
